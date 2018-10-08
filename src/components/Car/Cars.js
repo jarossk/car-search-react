a3d29498af09d5e5c3746e160dc6cc2e79bf5ad1
Jarek <klata.jarek@gmail.com>
2018-10-08 18:00:43 +0100

    Stateless component car

---
+++ src/components/Car/Cars.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const car = (props) => {
+    return (
+        <article className="Car" onClick={props.clicked}>
+            <h1>{props.makeName}</h1>
+            <div className="Info">
+                <p className="ModelName">{props.modelName}</p>
+            </div>
+        </article>
+    )
+}
